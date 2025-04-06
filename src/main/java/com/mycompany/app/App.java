1d96383e125506f59eba396d4bb3b1a17c9a93ea
RochaG07 <you@example.comguilhermerochapsilva@gmail.com>
2025-04-05 21:16:51 -0300

    Criação do menu inicial

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -3,9 +3,8 @@ package com.mycompany.app;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.io.StringReader;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 /**
  * Hello world!
@@ -14,6 +13,10 @@ public class App {
     public static void main(String[] args) {
 
         System.out.println(readQuestionsFromForm());
+
+        printMenuOptions();
+        int chosenOption = getOptionFromUser();
+
     }
 
     public static String readQuestionsFromForm(){
@@ -34,5 +37,42 @@ public class App {
         }
 
         return questions.toString();
+    }
+
+    public static boolean isValidOption(int option){
+        boolean isValid = option > 0 && option < 7;
+
+        if(!isValid){
+            System.out.print("Insira uma opção válida.\n");
+        }
+
+        return isValid;
+    }
+
+    public static void printMenuOptions(){
+        System.out.print("1. Cadastrar um novo pet\n");
+        System.out.print("2. Alterar dados do pet cadastrado\n");
+        System.out.print("3. Deletar pet cadastrado\n");
+        System.out.print("4. Listar todos os pets cadastrados\n");
+        System.out.print("5. Listar pets por algum critério (idade, nome, raça)\n");
+        System.out.print("6. Sair\n");
+    }
+
+    public static int getOptionFromUser(){
+        Scanner sc = new Scanner(System.in);
+
+        int res = 0;
+        do {
+            System.out.print("\nDigite a opção desejada: ");
+
+            try {
+                res = Integer.parseInt(sc.next());
+            } catch (NumberFormatException ignored){}
+
+        } while (!isValidOption(res));
+
+        sc.close();
+
+        return res;
     }
 }
